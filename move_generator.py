f68605cef67eee33a6ad348c399a9b192f7f12d0
Jaiesh Badireddy <jaiesh.badireddy@gmail.com>
2026-08-31 22:46:09 +0300

    Update move_generator.py
    game over

--- move_generator.py
+++ move_generator.py
@@ -18,19 +18,30 @@ def get_successors(game):
         successors.append((move, next_game))
     return successors
 
+def is_game_over(game):
+    if game.unplaced_men[1] != 0 or game.unplaced_men[2] != 0:
+        return False
+    if game.placed_men[1] < 3 or game.placed_men[2] < 3:
+        return True
+    if game.draw_limit <= 0:
+        return True
+    if len(get_successors(game)) == 0:
+        return True
+    return False
+
 if __name__ == "__main__":
-    game = Game_rules()#placement
+    game = Game_rules()
     new_game = apply_move(game, 0)
     print("Placement test:", new_game.board[0] == 1)
 
-    game2 = Game_rules()#movement
+    game2 = Game_rules()
     game2.unplaced_men = {1: 0, 2: 0}
     game2.placed_men = {1: 9, 2: 9}
     game2.board[0] = 1
     new_game2 = apply_move(game2, (0, 1))
     print("Movement test:", new_game2.board[0] == 0 and new_game2.board[1] == 1)
 
-    game3 = Game_rules()#removal
+    game3 = Game_rules()
     game3.remove = True
     game3.board[5] = 2
     game3.current_player = 1
@@ -40,3 +51,22 @@ if __name__ == "__main__":
 
     successors = get_successors(game)
     print("How many possible next moves:", len(successors))
+
+    game_fly = Game_rules()
+    game_fly.unplaced_men = {1: 0, 2: 0}
+    game_fly.placed_men = {1: 3, 2: 9}
+    game_fly.board[0] = 1
+    successors_fly = get_successors(game_fly)
+    print("Flying test (should be 23):", len(successors_fly))
+
+    game_trap = Game_rules()
+    game_trap.unplaced_men = {1: 0, 2: 0}
+    game_trap.placed_men = {1: 4, 2: 9}
+    game_trap.board[0] = 1
+    game_trap.board[1] = 2
+    game_trap.board[9] = 2
+    successors_trap = get_successors(game_trap)
+    print("Trapped test (should be 0):", len(successors_trap))
+
+    print("Game over test 1 (new game, should be False):", is_game_over(game))
+    print("Game over test 2 (trapped, should be True):", is_game_over(game_trap))
